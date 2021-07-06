a8dabea31278cadaf39008273eea2e1c0d590ec8
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-07-06 19:12:02 -0300

    add: TestComplete scenarios and project integrated with CucumberStudio

---
+++ Scenarios/FakeEcommercePoc.feature
@@ -0,0 +1,14 @@
+﻿Feature: FakeEcommercePoc
+    As Mario, the user
+    I would like to search for buying clothes
+    For not freeze
+
+  Scenario: Search for valid product
+    Given Mario is on the Homepage
+    When Mario searches for "dress"
+    Then Mario should see search results
+
+  Scenario: Search for invalid product
+    Given Mario is on the Homepage
+    When Mario searches for "potato"
+    Then Mario should not see search results
---
+++ Script/StepDefinitions.py
@@ -0,0 +1,23 @@
+﻿@given("Mario is on the Homepage")
+def step_impl():
+  browserWindow = Aliases.browser.BrowserWindow
+  Browsers.Item[btChrome].Navigate("http://automationpractice.com/index.php")
+  browserWindow.Maximize()
+  aqObject.CheckProperty(Aliases.browser.pageMyStore.header.formSearchbox.textboxSearchQueryTop, "Exists", cmpEqual, True)
+
+@when("Mario searches for {arg}")
+def step_impl(search_term):
+  browser = Aliases.browser
+  textbox = browser.pageMyStore.header.formSearchbox
+  textbox2 = textbox.textboxSearchQueryTop
+  textbox2.Click()
+  textbox2.SetText(search_term)
+  textbox.buttonSearch.ClickButton()
+
+@then("Mario should see search results")
+def step_impl():
+  aqObject.CheckProperty(Aliases.browser.pageIndex.textnode.textnodeQuickView289830515Printe, "Exists", cmpEqual, True)
+
+@then("Mario should not see search results")
+def step_impl():
+  aqObject.CheckProperty(Aliases.browser.pageIndex.textnodeNoResultsWereFoundForYou, "contentText", cmpContains, "No results were found for your search")
